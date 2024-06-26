4462f7f0a0110f559939b1c7880bedbd05c757c3
Amine-Triki <amine.triki15@gmail.com>
2024-06-26 14:28:22 +0100

    adding pwa

--- resources/views/show_Map.blade.php
+++ resources/views/show_Map.blade.php
@@ -54,6 +54,10 @@
             overflow: scroll;
         }
     </style>
+    <!-- PWA  -->
+<meta name="theme-color" content="#6777ef"/>
+<link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
+<link rel="manifest" href="{{ asset('/manifest.json') }}">
 </head>
 <body>
     <main>
@@ -131,6 +135,14 @@
                     .catch(error => console.error('Error:', error));
             });
         });
+    </script>
+    <script src="{{ asset('/sw.js') }}"></script>
+    <script>
+        if (!navigator.serviceWorker.controller) {
+            navigator.serviceWorker.register("/sw.js").then(function (reg) {
+                console.log("Service worker has been registered for scope: " + reg.scope);
+            });
+        }
     </script>
 </body>
 </html>
